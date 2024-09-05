de5fe044a032d8cbaa64d43b63ca76b346504bb2
Csáki Bence <KarolyBen>
2024-09-05 12:19:39 +0200

    hibák kijavítása

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; //Rosszul van írva
-    std::cout << '1-100 ertekek duplazasa' //hiányzó ;
-    for (int i = 0;)    //rossz for loop
+    int *b = new int[N_ELEMENTS];
+    std::cout << "1-100 ertekek duplazasa" <<std::endl;
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)     //rossz for loop
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        std::cout << "Ertek:"   //hiányzó ;
+        std::cout << "Ertek:" << std::endl;
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;  //nincs kezdőérték
-    for (int i = 0; i < N_ELEMENTS, i++)    // , helyeett ;
+    int atlag=0;
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        atlag += b[i]   //hiányzó ;
+        atlag += b[i];
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
